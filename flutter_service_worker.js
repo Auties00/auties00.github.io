b33c5b0bd761ec0416afa274feecedf4e10c2f49
Auties00 <alautiero@gmail.com>
2020-06-18 19:17:16 +0200

    Bug Fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "564455a3f1339926aa5dd5c99673e5f6",
 "/": "564455a3f1339926aa5dd5c99673e5f6",
-"main.dart.js": "0c06a28e4c8a95416d893afa0f3a6521",
+"main.dart.js": "872ebba71d53decb3f6636cf525da161",
 "manifest.json": "a9a6fc8d1fe311548f76cb15ce98b711"
 };
 
