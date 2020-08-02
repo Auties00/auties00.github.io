1fcd52fe9889a1a3f7e2354015a8e1773a08cfbd
Auties00 <alautiero@gmail.com>
2020-08-02 22:49:05 +0200

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7ff6193210eb9edf87b7b791dfab3684",
 "/": "7ff6193210eb9edf87b7b791dfab3684",
-"main.dart.js": "23d543530346e3288c63c528d0b56c77",
+"main.dart.js": "d72ad1fb49f7239fcef0ad7ed45548b2",
 "manifest.json": "a9a6fc8d1fe311548f76cb15ce98b711",
 "style.css": "4c1de4aef291bf3339666410a8dd5053"
 };
