2199d6b46b195ea8f34d4e8d75fb8689f2ca2725
Alessandro Autiero <alautiero@gmail.com>
2023-01-04 16:30:00 +0100

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,9 +47,9 @@ const RESOURCES = {
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "870455315da1b99b8c8a6691d2f54611",
-"/": "870455315da1b99b8c8a6691d2f54611",
-"main.dart.js": "1d04a0c3a863d8b7eb41fb9859778c8b",
+"index.html": "6f7cc2235f0484ac2634c66125b9a603",
+"/": "6f7cc2235f0484ac2634c66125b9a603",
+"main.dart.js": "5f7934882436dd09c575c4e32f0fecd1",
 "manifest.json": "a9a6fc8d1fe311548f76cb15ce98b711",
 "style.css": "1c4781bf6604b687320c4b0cdcca3ef9",
 "version.json": "5ab905c7d4f26c8b56f55bb966102575"
